5bd3bf2087de0df6507dc8df2da04a899fecd266
Your Name <you@example.com>
2018-08-07 13:59:11 +0800

    87

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -33,10 +33,7 @@ public class UserController {
         ServerRespose<User> response = iUserService.login(username, password);
         if (response.isSuccess()) {
             //session.setAttribute(Const.CURRENT_USER, response.getData());
-            //tewe
             CookieUtil.writeLoginToken(httpServletResponse,session.getId());
-            CookieUtil.readLonginToken(httpServletRequest);
-            CookieUtil.delLoginToken(httpServletRequest,httpServletResponse);
             RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()),Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
         }
         return response;
--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -39,6 +39,7 @@ public class CookieUtil {
         ck.setDomain(COOKIE_DOMAIN);
         /* "/"意思是说只有根目录及其子目录下的代码和页面可以获取到这个cookie，如果换成test，那么就test目录下或其子目录可以获取到cookie */
         ck.setPath("/");
+        ck.setHttpOnly(true);//提高安全性防止脚本攻击
         /* 单位是秒,如果这个maxage不设置的话，cookie是不会写入硬盘的，而是写入内存。只有当前页面有效 */
         ck.setMaxAge(60 * 60 *24 * 365);
         log.info("write cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
