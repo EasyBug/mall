6a240760f4c51769b6948839f05115442e01f7ba
Your Name <you@example.com>
2018-08-08 14:58:14 +0800

    99

--- src/main/java/com/mmall/controller/common/SessionExpireFilter.java
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -25,15 +25,16 @@ public class SessionExpireFilter implements Filter{
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletRequest httpServletRequest = (HttpServletRequest)servletRequest;
+
         String loginToken = CookieUtil.readLonginToken(httpServletRequest);
+
         if(StringUtils.isNotEmpty(loginToken)){
-            /*
-            * 先判断logintoken是否为空或者""
-            * 如果不为空，符合条件，继续拿user信息
-            * * */
+            //判断logintoken是否为空或者""；
+            //如果不为空的话，符合条件，继续拿user信息
+
             String userJsonStr = RedisPoolUtil.get(loginToken);
-            String user = JsonUtil.string2Obj(userJsonStr, User.class);
+            User user = JsonUtil.string2Obj(userJsonStr,User.class);
             if(user != null){
                 //如果user不为空，则重置session的时间，即调用expire命令
                 RedisPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
